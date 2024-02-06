12c538b502d3e483c10776add82d1cc66711f239
cozyCodr <brightlonda.scholar@gmail.com>
2024-02-06 08:38:54 +0200

    removed whitespace before colon

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(long int));
 	printf("Size of a long ");
-	printf("long int : %lu byte(s)\n", (unsigned long) sizeof(long long int));
+	printf("long int: %lu byte(s)\n", (unsigned long) sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(float));
 	return (0);
 }
