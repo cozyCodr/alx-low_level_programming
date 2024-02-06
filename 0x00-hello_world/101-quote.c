451ec0293b02c145ba40eb8a5ce0985127337f23
cozyCodr <brightlonda.scholar@gmail.com>
2024-02-06 08:53:57 +0200

    hot fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, ");
-	fprintf("2015-10-19\n");
-	return (0);
+	fprintf(stderr, "2015-10-19\n");
+	return (1);
 }
